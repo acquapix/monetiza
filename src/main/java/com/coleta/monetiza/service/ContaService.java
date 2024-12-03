3d64d14fa49c1b35cc4ba84317810fb8ae0c0c0f
joaopmaximo <jp.maximoo@gmail.com>
2024-12-03 02:10:33 -0300

    feat: add geracao de tokens

--- src/main/java/com/coleta/monetiza/service/ContaService.java
+++ src/main/java/com/coleta/monetiza/service/ContaService.java
@@ -10,16 +10,26 @@ import com.coleta.monetiza.repository.ContaRepository;
 public class ContaService extends AbstractService<Conta> {
 
 	private TokenService tokenService;
-	
+
 	public ContaService(ContaRepository repository, TokenService tokenService) {
 		super(repository);
 		this.tokenService = tokenService;
 	}
 
 	public void depositar(Conta conta, double valor) {
-		double novoSaldo = conta.getSaldo() + valor;
+		double antigoSaldo = conta.getSaldo();
+		int tokensAntes = (int) (antigoSaldo / 5);
+
+		double novoSaldo = antigoSaldo + valor;
+		int tokensDepois = (int) (novoSaldo / 5);
+
 		conta.setSaldo(novoSaldo);
 		update(conta);
+
+		for (int i = 0; i < (tokensDepois - tokensAntes); i++) {
+			var token = new Token(conta);
+			tokenService.create(token);
+		}
 	}
 
 	public void sacar(Conta conta, double valor) {
