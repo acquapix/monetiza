66ff15fc254a6de09a535e2e95d6f73a22119c1f
joaopmaximo <jp.maximoo@gmail.com>
2024-12-03 01:28:00 -0300

    feat: add token files

--- src/main/java/com/coleta/monetiza/service/ContaService.java
+++ src/main/java/com/coleta/monetiza/service/ContaService.java
@@ -3,13 +3,17 @@ package com.coleta.monetiza.service;
 import org.springframework.stereotype.Service;
 
 import com.coleta.monetiza.model.Conta;
+import com.coleta.monetiza.model.Token;
 import com.coleta.monetiza.repository.ContaRepository;
 
 @Service
 public class ContaService extends AbstractService<Conta> {
+
+	private TokenService tokenService;
 	
-	public ContaService(ContaRepository repository) {
+	public ContaService(ContaRepository repository, TokenService tokenService) {
 		super(repository);
+		this.tokenService = tokenService;
 	}
 
 	public void depositar(Conta conta, double valor) {
